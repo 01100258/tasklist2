f650126cbdc1a63887450751929645ed2d375050
jiro.kirameki <qualial2014@gmail.com>
2018-04-14 04:32:44 +0000

    user registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WelcomeController@index');
+
+//Route::resource('tasks', 'TasksController');
+
